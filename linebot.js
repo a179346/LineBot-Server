1db23db265c86c94a510c3287b24b18d60ef9a1b
hsiang <hsiang@borden.com.tw>
2020-08-07 15:37:54 +0800

    reply when bot name is called
    Signed-off-by: hsiang <hsiang@borden.com.tw>

--- linebot.js
+++ linebot.js
@@ -5,9 +5,40 @@ const config = {
   channelAccessToken: process.env.channelAccessToken,
 };
 const bot = linebot(config);
-
+const botName = process.env.BOT_NAME;
 module.exports = bot;
 
-bot.on('message', function (event) {
-  console.log(event);
+bot.on('message', async function (event) {
+  try {
+    if (event.type === 'message' && event.message && event.message.text) {
+      const message = event.message.text;
+      if (message === botName) {
+        await event.reply('What trouble you bring now?');
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    await event.reply('Unknown Error  (x_x)');
+  }
 });
+// event
+// {
+//   type: 'message',
+//   replyToken: 'replyToken',
+//   source: {
+//     userId: 'userId',
+//     groupId: 'groupId',
+//     type: 'group',
+//     profile: [Function],
+//     member: [Function]
+//   },
+//   timestamp: 1596785042340,
+//   mode: 'active',
+//   message: {
+//     type: 'text',
+//     id: '12456661773182',
+//     text: 'Hello',
+//     content: [Function]
+//   },
+//   reply: [Function]
+// }
--- system/initEnv.js
+++ system/initEnv.js
@@ -3,3 +3,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'local';
 process.env.channelId = process.env.channelId || 'channelId';
 process.env.channelSecret = process.env.channelSecret || 'channelSecret';
 process.env.channelAccessToken = process.env.channelAccessToken || 'channelAccessToken';
+process.env.BOT_NAME = process.env.BOT_NAME || 'my bot name';
